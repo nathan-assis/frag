9a713ed08fcdb8257c60bb08bc651a52596bb9f8
nathanassis <75035881+nathanassis@users.noreply.github.com>
2025-07-13 13:55:18 -0300

    feat: create database methods

---
+++ src/database/repository.py
@@ -0,0 +1,24 @@
+from pymilvus import MilvusClient
+
+import src.database.consts as Constants
+from src.database.client import Client
+from src.schemas.file import File as FileSchema
+
+
+class Repository:
+    def __init__(self):
+        self._client: MilvusClient = Client().client()
+
+    def upsert(self, data: list[FileSchema]) -> None:
+        self._client.upsert(collection_name=Constants.COLLECTION, data=data)
+
+    def search(self, query_vector: list[float], top_k: int = 5) -> list[dict]:
+        results = self._client.search(
+            collection_name=Constants.COLLECTION,
+            data=query_vector,
+            limit=top_k,
+            output_fields=["path", "file_name", "text", "chunks"],
+            search_params={"metric_type": "COSINE", "params": {"nprobe": 10}},
+        )
+
+        return results[0]
--- src/services/rag_pipeline.py
+++ src/services/rag_pipeline.py
@@ -1,29 +1,21 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
-from src.schemas.folder import Folder as FolderSchema
+from src.database.repository import Repository
 from src.services.file import File
 
 
 class RAGPipeline:
-    __folder: FolderSchema = None
-
     @staticmethod
     def process_folder(path_str: str) -> None:
-        if RAGPipeline.__folder is None:
-            RAGPipeline.__folder = FolderSchema(path=Path(path_str), data=[])
+        files = [file for file in list(Path(path_str).rglob("*")) if file.is_file()]
 
-        files = [
-            file
-            for file in list(RAGPipeline.__folder.path.rglob("*"))
-            if file.is_file()
-        ]
-
-        RAGPipeline.__folder.data = []
+        data = []
         with ThreadPoolExecutor() as executor:
             futures = [executor.submit(File, file) for file in files]
 
             for future in as_completed(futures):
-                RAGPipeline.__folder.data.append(future.result().get_file())
+                data.append(future.result().get_file())
 
-        # TODO: save to milvus
+        db = Repository()
+        db.upsert(data)
